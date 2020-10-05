40108d233a12ee69c84e04d2ac677bf0b38139d5
QiluHuang <lunahuang@foxmail.com>
2020-10-05 01:18:33 -0700

    html-assignment

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -25,48 +25,44 @@ const Article = () => {
         </p> 
       </header>
 
-      
-
-      
-
 
       <section>
-      <h2> The history of the Statue of Liberty </h2>
-      <p>
-      Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of <i> Outstanding Universal Value </i>.
-      </p>
-      <p>
-      Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel -- yes, the same guy who built Paris's Eiffel Tower.
-      </p>
-      <p>
-      Lady Liberty was a gift from France to the United States to celebrate the centennial of their nation's independence -- although, somewhat awkwardly, she arrived a decade too late. Nevertheless, President Grover Cleveland formally unveiled her in 1886.
-      </p>
-      <p>
-      Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.
-      </p>
+        <h2> The history of the Statue of Liberty </h2>
+        <p>
+        Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of <i> Outstanding Universal Value </i>.
+        </p>
+        <p>
+        Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel -- yes, the same guy who built Paris's Eiffel Tower.
+        </p>
+        <p>
+        Lady Liberty was a gift from France to the United States to celebrate the centennial of their nation's independence -- although, somewhat awkwardly, she arrived a decade too late. Nevertheless, President Grover Cleveland formally unveiled her in 1886.
+        </p>
+        <p>
+        Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.
+        </p>
 
-      <p>
-      "The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal: 
-      </p>
+        <p>
+        "The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal: 
+        </p>
 
-      <blockquote>Give me your tired, your poor <br></br> Your huddled masses yearning to breathe free.</blockquote>
+        <blockquote>Give me your tired, your poor <br></br> Your huddled masses yearning to breathe free.</blockquote>
 
-      <p>
-      In 2017, nearly 4.5 million people visited Liberty Island.
-      </p>
-      <p>
-      When completed, the Statue of Liberty Museum will be 26,000 square feet of history, inspiration and deeper analysis of the statue's symbolism both in the US and around the world.
-      </p>
-      <p>
-      The inspiration gallery, which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower Manhattan, including the World Trade Center.
-      </p>
+        <p>
+        In 2017, nearly 4.5 million people visited Liberty Island.
+        </p>
+        <p>
+        When completed, the Statue of Liberty Museum will be 26,000 square feet of history, inspiration and deeper analysis of the statue's symbolism both in the US and around the world.
+        </p>
+        <p>
+        The inspiration gallery, which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower Manhattan, including the World Trade Center.
+        </p>
       </section>
 
 
       <section>
         <h2> Some facts about the Statue of Liberty </h2>
         <ul>
-          <li> Total overall height from the base of the pedestal foundation to the tip of the torch is 305 feet, 6 inches </li>
+          <li> Total overall height from the base of the pedestal foundation to the tip of the torch is <b> 305 feet, 6 inches </b></li>
           <li> Height of the Statue from her heel to the top of her head is 111 feet, 6 inches </li>
           <li> The Statue has a <b> 35-foot </b> waistline </li>
           <li> Total weight of the Statue of Liberty is <b> 225 tons (or 450,000 pounds) </b></li>
