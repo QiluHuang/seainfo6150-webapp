0223308fb6bfb091e7ef37aca1358b25a1a55a70
QiluHuang <lunahuang@foxmail.com>
2020-11-22 22:12:47 -0800

    images

--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./ArticleListItem.module.css";
 import ArticleTextToggleButton from "../ArticleTextToggleButton/ArticleTextToggleButton.jsx";
+import ArticleImage from "../ArticleImage/ArticleImage";
 
 const ArticleListItem = (props) => {
   const [isTextShowing, setIsTextShowing] = useState(false);
@@ -14,6 +15,9 @@ const ArticleListItem = (props) => {
     <li className={styles.container}>
       <article className={styles.article}>
         <div className={styles.wrapper}>
+        <div className = {styles.upper_image}>
+        <ArticleImage url = {props.article.image._url} title={props.article.title}/>
+        </div>
           <h2 className={styles.title}>{props.article.title}</h2>
           {isTextShowing && (
             <div className={styles.text}>
@@ -23,6 +27,7 @@ const ArticleListItem = (props) => {
               </time>
             </div>
           )}
+          
         </div>
         <ArticleTextToggleButton
           buttonText={isTextShowing ? "Show less" : "Show more"}
